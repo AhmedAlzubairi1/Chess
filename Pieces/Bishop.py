932072e62e602e5a3fc14b1aefad7b282a907cf1
Ahmed Alzubairi <afa2135@columbia.edu>
2020-10-01 17:24:38 -0400

    Added Bishop Piece

--- Pieces/Bishop.py
+++ Pieces/Bishop.py
@@ -8,5 +8,55 @@ class Pawn(Piece):
     def availableMoves(self):
         #Returns a set of the avaiable moves the Bishop can make
         moves=set()
-        #
+        tempRow=self.row+1
+        tempCol=self.possibleCol[self.col]-1
+        #Top left diagonal 
+        while 0<=tempCol<=7 and 0<=tempRow<=7:
+            if self.board[tempRow][tempCol] is None:
+                moves.add((tempRow,tempCol))
+            else:
+                if self.validMove(tempRow,tempCol):
+                    moves.add((tempRow,tempCol))
+                break
+            tempRow+=1
+            tempCol-=1
+
+        #Top right diagonal 
+        tempRow=self.row+1
+        tempCol=self.possibleCol[self.col]+1
+        while 0<=tempCol<=7 and 0<=tempRow<=7:
+            if self.board[tempRow][tempCol] is None:
+                moves.add((tempRow,tempCol))
+            else:
+                if self.validMove(tempRow,tempCol):
+                    moves.add((tempRow,tempCol))
+                break
+            tempRow+=1
+            tempCol+=1
+
+        #Bottom left diagonal 
+        tempRow=self.row-1
+        tempCol=self.possibleCol[self.col]-1
+        while 0<=tempCol<=7 and 0<=tempRow<=7:
+            if self.board[tempRow][tempCol] is None:
+                moves.add((tempRow,tempCol))
+            else:
+                if self.validMove(tempRow,tempCol):
+                    moves.add((tempRow,tempCol))
+                break
+            tempRow-=1
+            tempCol-=1
+
+        #Bottom right diagonal 
+        tempRow=self.row-1
+        tempCol=self.possibleCol[self.col]+1
+        while 0<=tempCol<=7 and 0<=tempRow<=7:
+            if self.board[tempRow][tempCol] is None:
+                moves.add((tempRow,tempCol))
+            else:
+                if self.validMove(tempRow,tempCol):
+                    moves.add((tempRow,tempCol))
+                break
+            tempRow-=1
+            tempCol+=1
         return moves
