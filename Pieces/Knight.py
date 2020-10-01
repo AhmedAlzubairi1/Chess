ff255f286b3c5142fd6abf4835689522731b207b
Ahmed Alzubairi <afa2135@columbia.edu>
2020-10-01 18:07:32 -0400

    Added the Knight Piece

--- Pieces/Knight.py
+++ Pieces/Knight.py
@@ -3,12 +3,17 @@ from Piece import Piece
 class Pawn(Piece):
     def __init__(self,board,color,group,row,col):
         super().__init__(board,color,group,"Bishop",row,col)
-        print(self.row)
 
     def availableMoves(self):
         #Returns a set of the avaiable moves the Bishop can make
         moves=set()
-        tempRow=self.row
+        #The knight moves in 8 different moves in 8 different L like moves
+        
+        tempRow=self.row-1
         tempCol=self.possibleCol[self.col]
-
+        #This is the L moves direction
+        direction=[(2,-1),(2,1),(-2,-1),(-2,1), (1,-2), (-1,-2), (1,2), (-1,2)]       ]
+        for (i,k) in direction:
+            if 0<=tempRow+i<=7 and 0<=tempCol+k<=7 and (self.board[tempRow+i][tempCol+k] is None or self.validMove(tempRow+i,tempCol+k)):
+                moves.add((tempRow+i,tempCol+k))            
         return moves
